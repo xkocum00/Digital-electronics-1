52b4c15803672423efca9fd421f769526291f3cf
xkocum00 <xkocum00@vutbr.cz>
2021-02-24 21:44:30 +0100

    upravy

---
+++ Labs/03-vivado/project_1comparator/project_1comparator.srcs/sim_1/new/tb_comparator_2bit.vhd
@@ -0,0 +1,132 @@
+------------------------------------------------------------------------
+--
+-- Testbench for 2-bit binary comparator.
+-- EDA Playground
+--
+-- Copyright (c) 2020-2021 Tomas Fryza
+-- Dept. of Radio Electronics, Brno University of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------------------
+entity tb_comparator_4bit is
+    -- Entity of testbench is always empty
+end entity tb_comparator_4bit;
+
+------------------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------------------
+architecture testbench of tb_comparator_4bit is
+
+    -- Local signals
+    signal s_a            : std_logic_vector(4 - 1 downto 0);
+    signal s_b            : std_logic_vector(4 - 1 downto 0);
+    signal s_B_greater_A  : std_logic;
+    signal s_B_equals_A   : std_logic;
+    signal s_B_less_A     : std_logic;
+
+begin
+    -- Connecting testbench signals with comparator_4bit entity (Unit Under Test)
+    uut_comparator_4bit : entity work.comparator_4bit
+        port map(
+            a_i           => s_a,
+            b_i           => s_b,
+            B_greater_A_o => s_B_greater_A,
+            B_equals_A_o  => s_B_equals_A,
+            B_less_A_o    => s_B_less_A
+        );
+
+    --------------------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------------------
+    p_stimulus : process
+    begin
+        -- Report a note at the begining of stimulus process
+        report "Stimulus process started" severity note;
+
+
+        -- First test values
+        s_b <= "0000"; s_a <= "0000"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '1') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0000" severity error;
+        
+        -- First test values
+        s_b <= "0000"; s_a <= "0001"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '0') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0001" severity error;
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "0010"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '1') and (s_B_equals_A = '0') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0010" severity error;
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "0011"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '0') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0011" severity error;
+        
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "0100"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '0') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0100" severity error;
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "0101"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '0') and (s_B_less_A = '0'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0101" severity error;
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "0110"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '0') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0110" severity error;
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "0111"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '1') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 0111" severity error;
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "1000"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '0') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 1000" severity error;
+        
+         -- First test values
+        s_b <= "0000"; s_a <= "1001"; wait for 100 ns;
+        -- Expected output
+        assert ((s_B_greater_A = '0') and (s_B_equals_A = '0') and (s_B_less_A = '1'))
+        -- If false, then report an error
+        report "Test failed for input combination: 0000, 1001" severity error;
+        
+        -- WRITE OTHER TESTS HERE
+
+
+        -- Report a note at the end of stimulus process
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+end architecture testbench;
