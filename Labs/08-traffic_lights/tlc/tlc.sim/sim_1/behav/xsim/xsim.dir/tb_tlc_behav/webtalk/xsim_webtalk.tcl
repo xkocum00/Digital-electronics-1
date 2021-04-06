6f0e7d9c37b583ac54ad6fb8a455743896d9d66a
xkocum00 <78788886+xkocum00@users.noreply.github.com>
2021-04-06 18:45:35 +0200

    xkocum00

--- Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/xsim_webtalk.tcl
+++ Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/Users/Kocum/Desktop/DE1/Kocum/Digital-electronics-1/Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Tue Apr  6 18:11:53 2021" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Tue Apr  6 18:45:21 2021" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "3064766" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "995562b9-c481-462e-aff2-b8caf2760eef" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "24ab4f5ac0f04b698cb942c5dcedb13a" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i5-7200U CPU @ 2.50GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "5 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "6404_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2034137018 -regid "" -xml C:/Users/Kocum/Desktop/DE1/Kocum/Digital-electronics-1/Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Kocum/Desktop/DE1/Kocum/Digital-electronics-1/Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Kocum/Desktop/DE1/Kocum/Digital-electronics-1/Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 1117500921 -regid "" -xml C:/Users/Kocum/Desktop/DE1/Kocum/Digital-electronics-1/Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Kocum/Desktop/DE1/Kocum/Digital-electronics-1/Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Kocum/Desktop/DE1/Kocum/Digital-electronics-1/Labs/08-traffic_lights/tlc/tlc.sim/sim_1/behav/xsim/xsim.dir/tb_tlc_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
